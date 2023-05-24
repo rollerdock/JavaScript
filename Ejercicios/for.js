27a9f183d7908bb8743441737c13ef831a937740
Rollerdock <jasplleida@gmail.com>
2023-05-24 12:35:21 +0200

    trabajando con classes

---
+++ Ejercicios/for.js
@@ -0,0 +1,15 @@
+const nombres = [
+  "Carlos",
+  "Cristian",
+  "chisthoper",
+  "Estefania",
+  "Erika",
+  "Manuel",
+];
+nombres.forEach((nombre) => {
+  console.log(nombre);
+});
+
+for (let numero = 1; numero < 11; numero++) {
+  console.log(numero);
+}
---
+++ Ejercicios/forinyforof.js
@@ -0,0 +1,6 @@
+const etiquetas = document.head.children;
+console.log(etiquetas);
+
+for (elento of etiquetas) {
+  console.log(elento);
+}
